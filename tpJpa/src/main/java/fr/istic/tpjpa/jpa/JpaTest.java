b106086dc16b44649c614c31eef22e32c60add81
barais <olivier.barais@gmail.com>
2013-01-22 12:09:31 +0100

    initial

---
+++ tpJpa/src/main/java/fr/istic/tpjpa/jpa/JpaTest.java
@@ -0,0 +1,58 @@
+package fr.istic.tpjpa.jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import fr.istic.tpjpa.domain.Department;
+import fr.istic.tpjpa.domain.Employee;
+
+public class JpaTest {
+
+	private EntityManager manager;
+
+	public JpaTest(EntityManager manager) {
+		this.manager = manager;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence
+				.createEntityManagerFactory("example");
+		EntityManager manager = factory.createEntityManager();
+		JpaTest test = new JpaTest(manager);
+
+		
+
+		// TODO create entity
+
+		for (int j = 0; j < 100; j++) {
+			EntityTransaction tx = manager.getTransaction();
+			tx.begin();
+			Department d = new Department("dep"+j);
+			manager.persist(d);
+			for (int i = 0; i < 10000; i++) {
+				Employee e = new Employee("emp_"+j + "_"+i);
+				
+				manager.persist(e);
+				e.setDepartment(d);
+				d.getEmployees().add(e);
+				
+				
+			}
+			tx.commit();
+			
+		}
+
+		// TODO persist entity
+
+
+		// TODO run request
+
+		System.out.println(".. done");
+	}
+
+}
---
+++ tpJpa/src/main/java/fr/istic/tpjpa/jpa/N1Select.java
@@ -0,0 +1,57 @@
+package fr.istic.tpjpa.jpa;
+
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import fr.istic.tpjpa.domain.Department;
+import fr.istic.tpjpa.domain.Employee;
+
+public class N1Select {
+
+	private EntityManager manager;
+
+	public N1Select(EntityManager manager) {
+		this.manager = manager;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence
+				.createEntityManagerFactory("withoutcreate");
+		EntityManager manager = factory.createEntityManager();
+		N1Select test = new N1Select(manager);
+
+		
+		TypedQuery<Department> q = test.manager.createQuery("select d from Departement where d.name='dep1'",Department.class);
+		long start = System.currentTimeMillis();
+		List<Department> res = q.getResultList();
+		
+		
+		for (Department d : res){
+			for (Employee e : d.getEmployees()){
+				System.err.println(e.getName());
+			}
+		}
+
+		long end = System.currentTimeMillis();
+		long duree = end - start;
+		System.err.println("temps d'exec = " +  duree);
+
+		// TODO persist entity
+
+
+		// TODO run request
+
+		System.out.println(".. done");
+	}
+
+}
